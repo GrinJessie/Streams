7bf8f094479efae43f8193d68dacec04e091e486
Lijing Wang <lijing@ualberta.ca>
2018-01-22 15:50:09 +0000

    made a request

--- https_example.js
+++ https_example.js
@@ -1,3 +1,16 @@
 var https = require('https');
+var options = {
+  host: 'www.example.org',
+  path: '/'
+};
 
-console.log('I did it!');
+//called by https when the request is made.
+var callback = function() {
+  console.log('In response handler callback!');
+}
+
+console.log('I am about to make the request!');
+
+https.request(options, callback).end();
+
+console.log('I have made the request!');
